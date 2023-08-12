822356f3dfe8c61973ccb13bac4c3efd561a33fc
Milyaz Kamil <milyazkamil@gmail.com>
2023-08-12 15:08:32 +0300

    created an array containing the information of the people

---
+++ src/people.js
@@ -0,0 +1,40 @@
+import person1 from "./assets/images/person-1.png";
+import person2 from "./assets/images/person-2.png";
+import person3 from "./assets/images/person-3.png";
+import person4 from "./assets/images/person-4.png";
+import person5 from "./assets/images/person-5.png";
+
+const people = [
+  {
+    id: 1,
+    name: 'Milyaz Kamil',
+    age: 22,
+    img: person1,
+  },
+  {
+    id: 2,
+    name: 'Bertie Yates',
+    age: 28,
+    img: person2,
+  },
+  {
+    id: 3,
+    name: 'Hester Hogan',
+    age: 32,
+    img: person3,
+  },
+  {
+    id: 4,
+    name: 'Sean Walsh',
+    age: 34,
+    img: person4,
+  },
+  {
+    id: 5,
+    name: 'Lola Gardner',
+    age: 25,
+    img: person5,
+  },
+];
+
+export default people;
